705292dc0e3b77cc28974c33b6a5c864485aedde
brainysoon <brainysoon@gmail.com>
2017-05-14 20:48:42 +0800

    complete-multi-features-linear-regression

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -3,11 +3,11 @@
 %
 %  Instructions
 %  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear regression exercise. 
 %
-%  You will need to complete the following functions in this 
+%  This file contains code that helps you get started on the
+%  linear regression exercise.
+%
+%  You will need to complete the following functions in this
 %  exericse:
 %
 %     warmUpExercise.m
@@ -54,19 +54,18 @@ fprintf('Normalizing Features ...\n');
 % Add intercept term to X
 X = [ones(m, 1) X];
 
-
 %% ================ Part 2: Gradient Descent ================
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: We have provided you with the following starter
 %               code that runs gradient descent with a particular
-%               learning rate (alpha). 
+%               learning rate (alpha).
 %
-%               Your task is to first make sure that your functions - 
-%               computeCost and gradientDescent already work with 
+%               Your task is to first make sure that your functions -
+%               computeCost and gradientDescent already work with
 %               this starter code and support multiple variables.
 %
-%               After that, try running gradient descent with 
+%               After that, try running gradient descent with
 %               different values of alpha and see which one gives
 %               you the best result.
 %
@@ -85,7 +84,7 @@ fprintf('Running gradient descent ...\n');
 alpha = 0.01;
 num_iters = 400;
 
-% Init Theta and Run Gradient Descent 
+% Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
@@ -106,6 +105,16 @@ fprintf('\n');
 % not need to be normalized.
 price = 0; % You should change this
 
+% feature normalization
+H=[1650 3];
+H=H-mu;
+H=H./sigma;
+
+%重新构建 H
+H=[1 H];
+
+% 预测 predict
+price=H*theta;
 
 % ============================================================
 
@@ -120,12 +129,12 @@ pause;
 fprintf('Solving with normal equations...\n');
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: The following code computes the closed form 
+% Instructions: The following code computes the closed form
 %               solution for linear regression using the normal
-%               equations. You should complete the code in 
+%               equations. You should complete the code in
 %               normalEqn.m
 %
-%               After doing so, you should complete this code 
+%               After doing so, you should complete this code
 %               to predict the price of a 1650 sq-ft, 3 br house.
 %
 
@@ -151,9 +160,13 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
 
+%
+H=[1 1650 3];
+
+%预测
+price=H*theta;
 
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using normal equations):\n $%f\n'], price);
-
--- machine-learning-ex1/ex1/normalEqn.m
+++ machine-learning-ex1/ex1/normalEqn.m
@@ -1,6 +1,6 @@
 function [theta] = normalEqn(X, y)
-%NORMALEQN Computes the closed-form solution to linear regression 
-%   NORMALEQN(X,y) computes the closed-form solution to linear 
+%NORMALEQN Computes the closed-form solution to linear regression
+%   NORMALEQN(X,y) computes the closed-form solution to linear
 %   regression using the normal equations.
 
 theta = zeros(size(X, 2), 1);
@@ -12,8 +12,8 @@ theta = zeros(size(X, 2), 1);
 
 % ---------------------- Sample Solution ----------------------
 
-
-
+    tmp=inv(X'*X);
+    theta=tmp*X'*y;
 
 % -------------------------------------------------------------
 
